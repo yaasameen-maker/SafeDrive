a627a75799a370e11de58d09b9cbc42d4a408f24
yaasameen.perez <yaasameen.perez@pursuit.org>
2025-09-25 07:39:16 +0000

    fix: Correct file path for SafeDriveApp component

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SafeDriveApp from '../SafeDriveApp';
+import SafeDriveApp from './SafeDriveApp';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
